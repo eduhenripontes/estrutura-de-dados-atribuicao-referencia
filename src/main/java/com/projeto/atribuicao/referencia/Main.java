55475939b0a3d5f5983002881e3ca20358339143
eduhenripontes <edu.henri@gmail.com>
2021-12-30 19:16:28 -0300

    Estrutura de Dados - Atribuicao e Referencia

---
+++ src/main/java/com/projeto/atribuicao/referencia/Main.java
@@ -0,0 +1,21 @@
+package com.projeto.atribuicao.referencia;
+
+public class Main {
+    public static void main(String[] args) {
+        int intA = 1;
+        int intB = intA;
+
+        System.out.println("intA= " + intA + " intB= " + intB);
+        intA = 2;
+        System.out.println("intA= " + intA + " intB= " + intB);
+
+        MeuObj objA = new MeuObj(1);
+        MeuObj objB = objA;
+
+        System.out.println("ObjA= " + objA + " ObjB= " + objB);
+        objA.setNum(2);
+        System.out.println("ObjA= " + objA + " ObjB= " + objB);
+
+
+    }
+}
